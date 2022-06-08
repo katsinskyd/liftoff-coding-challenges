d53a703bf37248f61f7e5b161cca52f92d23021a
Kat Garnier <kathryn.garnier@gmail.com>
2022-06-08 19:50:28 -0400

    added previous prompts for consistency

--- CodingChallenge2.js
+++ CodingChallenge2.js
@@ -1,3 +1,13 @@
+// Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+
+// minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+
+// minMax([2334454, 5]) ➞ [5, 2334454]
+
+// minMax([1]) ➞ [1, 1]
+
+// All test arrays will have at least one element and are valid.
+
 function minMax(array) {
     let sortedArray = array.sort(function(a, b){return a - b});
     return `[${sortedArray[0]}, ${sortedArray.pop()}]`;
